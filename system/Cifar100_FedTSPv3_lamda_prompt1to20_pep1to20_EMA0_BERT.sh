c09e74dbfd013ec9f53e51bb20ded0e396b9ba4c
XinghaoWu <wuxinghao@buaa.edu.cn>
2024-12-10 20:13:44 +0800

    improve training shell scipt on CIFAR10 and CIFAR100

--- system/Cifar100_FedTSPv3_lamda_prompt1to20_pep1to20_EMA0_BERT.sh
+++ system/Cifar100_FedTSPv3_lamda_prompt1to20_pep1to20_EMA0_BERT.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
 # 从命令行解析参数
-alpha=$1
-num_clients=$2
-model_family=$3
-lamda=$4
-batch_size=${5:-100}
-seed=${6:-0}
-device_id=${7:-0}
+noniid=$1
+alpha=$2
+num_clients=$3
+model_family=$4
+lamda=$5
+batch_size=${6:-100}
+seed=${7:-0}
+device_id=${8:-0}
 
 echo "参数列表:"
 echo "alpha: ${alpha}"
@@ -18,14 +19,36 @@ echo "batch_size: ${batch_size}"
 echo "seed: ${seed}"
 echo "device_id: ${device_id}"
 
-if [ -z "$alpha" ] || [ -z "$num_clients" ] || [ -z "$model_family" ] || [ -z "$lamda" ] || [ -z "$batch_size" ] || [ -z "$seed" ]; then
-  echo "请提供 alpha, num_clients, model_family, lamda 和 seed 参数，例如："
-  echo "./script.sh 0.1 20 HtFE2 7 0"
+if [ -z "$noniid" ] || [ -z "$alpha" ] || [ -z "$num_clients" ] || [ -z "$model_family" ] || [ -z "$lamda" ] || [ -z "$batch_size" ] || [ -z "$seed" ]; then
+  echo "请提供 noniid alpha, num_clients, model_family, lamda 和 seed 参数，例如："
+  echo "./script.sh dir 0.1 20 HtFE2 7 100 0 0"
   exit 1
 fi
 
 # 动态生成 dataset 参数
-dataset="Cifar10_dir_${alpha}_balance_${num_clients}"
+dataset="Cifar100_${noniid}_${alpha}_balance_${num_clients}"
+
+python main.py --dataset=${dataset} \
+          --num_classes=100 \
+          --model_family=${model_family} \
+          --local_learning_rate=0.01 \
+          --global_rounds=100 \
+          --algorithm=FedTSPv3 \
+          --local_epochs=5 \
+          --batch_size=${batch_size} \
+          --num_clients=${num_clients} \
+          --prompt_lr=0.01 \
+          --prompt_epoch=0 \
+          --lamda=${lamda} \
+          --len_prompt=0 \
+          --vision_proto=0 \
+          --EMA_alpha=0 \
+          --server_model=bert \
+          --feature_dim=768 \
+          --prompt_random_init \
+          --seed=${seed} \
+          --device_id=${device_id} \
+          --save_model=0 &
 
 for lamda in ${lamda};
 do
@@ -36,7 +59,7 @@ do
       for EMA_alpha in 0;
       do
         python main.py --dataset=${dataset} \
-          --num_classes=10 \
+          --num_classes=100 \
           --model_family=${model_family} \
           --local_learning_rate=0.01 \
           --global_rounds=100 \
@@ -58,7 +81,7 @@ do
           --save_model=0 &
 
         python main.py --dataset=${dataset} \
-          --num_classes=10 \
+          --num_classes=100 \
           --model_family=${model_family} \
           --local_learning_rate=0.01 \
           --global_rounds=100 \
--- system/Cifar10_FedTSPv3_lamda_prompt1to20_pep1to20_EMA0_BERT.sh
+++ system/Cifar10_FedTSPv3_lamda_prompt1to20_pep1to20_EMA0_BERT.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
 # 从命令行解析参数
-alpha=$1
-num_clients=$2
-model_family=$3
-lamda=$4
-batch_size=${5:-100}
-seed=${6:-0}
-device_id=${7:-0}
+noniid=$1
+alpha=$2
+num_clients=$3
+model_family=$4
+lamda=$5
+batch_size=${6:-100}
+seed=${7:-0}
+device_id=${8:-0}
 
 echo "参数列表:"
 echo "alpha: ${alpha}"
@@ -18,18 +19,40 @@ echo "batch_size: ${batch_size}"
 echo "seed: ${seed}"
 echo "device_id: ${device_id}"
 
-if [ -z "$alpha" ] || [ -z "$num_clients" ] || [ -z "$model_family" ] || [ -z "$lamda" ] || [ -z "$batch_size" ] || [ -z "$seed" ]; then
-  echo "请提供 alpha, num_clients, model_family, lamda 和 seed 参数，例如："
-  echo "./script.sh 0.1 20 HtFE2 7 0"
+if [ -z "$noniid" ] || [ -z "$alpha" ] || [ -z "$num_clients" ] || [ -z "$model_family" ] || [ -z "$lamda" ] || [ -z "$batch_size" ] || [ -z "$seed" ]; then
+  echo "请提供 noniid alpha, num_clients, model_family, lamda 和 seed 参数，例如："
+  echo "./script.sh dir 0.1 20 HtFE2 7 100 0 0"
   exit 1
 fi
 
 # 动态生成 dataset 参数
-dataset="Cifar10_dir_${alpha}_balance_${num_clients}"
+dataset="Cifar10_${noniid}_${alpha}_balance_${num_clients}"
+
+python main.py --dataset=${dataset} \
+          --num_classes=10 \
+          --model_family=${model_family} \
+          --local_learning_rate=0.01 \
+          --global_rounds=100 \
+          --algorithm=FedTSPv3 \
+          --local_epochs=5 \
+          --batch_size=${batch_size} \
+          --num_clients=${num_clients} \
+          --prompt_lr=0.01 \
+          --prompt_epoch=0 \
+          --lamda=${lamda} \
+          --len_prompt=0 \
+          --vision_proto=0 \
+          --EMA_alpha=0 \
+          --server_model=bert \
+          --feature_dim=768 \
+          --prompt_random_init \
+          --seed=${seed} \
+          --device_id=${device_id} \
+          --save_model=0 &
 
 for lamda in ${lamda};
 do
-  for prompt_epoch in 1 5 10;
+  for prompt_epoch in 1 5 10 15 20;
   do
     for len_prompt in 1 5 10 15 20;
     do
@@ -39,7 +62,7 @@ do
           --num_classes=10 \
           --model_family=${model_family} \
           --local_learning_rate=0.01 \
-          --global_rounds=60 \
+          --global_rounds=100 \
           --algorithm=FedTSPv3 \
           --local_epochs=5 \
           --batch_size=${batch_size} \
@@ -50,8 +73,8 @@ do
           --len_prompt=${len_prompt} \
           --vision_proto=0 \
           --EMA_alpha=${EMA_alpha} \
-          --server_model=clip \
-          --feature_dim=512 \
+          --server_model=bert \
+          --feature_dim=768 \
           --prompt_random_init \
           --seed=${seed} \
           --device_id=${device_id} \
@@ -61,7 +84,7 @@ do
           --num_classes=10 \
           --model_family=${model_family} \
           --local_learning_rate=0.01 \
-          --global_rounds=60 \
+          --global_rounds=100 \
           --algorithm=FedTSPv3 \
           --local_epochs=5 \
           --batch_size=${batch_size} \
@@ -72,8 +95,8 @@ do
           --len_prompt=${len_prompt} \
           --vision_proto=0 \
           --EMA_alpha=${EMA_alpha} \
-          --server_model=clip \
-          --feature_dim=512 \
+          --server_model=bert \
+          --feature_dim=768 \
           --seed=${seed} \
           --device_id=${device_id} \
           --save_model=0 &
