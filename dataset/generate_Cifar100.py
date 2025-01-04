a2769ef3f38988aa68ae37007daab02364e4961b
武星皓 <956540778@qq.com>
2025-01-04 15:13:44 +0800

    Add some files from the dataset to version control with Git.

---
+++ dataset/generate_Cifar100.py
@@ -0,0 +1,116 @@
+# PFLlib: Personalized Federated Learning Algorithm Library
+# Copyright (C) 2021  Jianqing Zhang
+
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+import numpy as np
+import os
+import sys
+import random
+import torch
+import torchvision
+import torchvision.transforms as transforms
+from utils.dataset_utils import check, separate_data, split_data, save_file
+
+import argparse
+def args_parser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--niid', type=str, default="noniid", help="non-iid distribution")
+    parser.add_argument('--balance', type=str, default="balance", help="balance data size per client")
+    parser.add_argument('--partition', type=str, default="dir", help="partition distribution, dir|pat")
+    parser.add_argument('--num_users', type=int, default=40, help="number of users")
+    parser.add_argument('--alpha', type=float, default=1.0, help="the degree of imbalance")
+
+
+
+    parser.add_argument('--seed', type=int, default=1, help="random seed")
+
+    args = parser.parse_args()
+    args.alpha = args.alpha if args.partition == 'dir' else int(args.alpha)
+    return args
+
+
+# Allocate data to users
+def generate_dataset(niid, balance, partition, args):
+    random.seed(args.seed)
+    np.random.seed(args.seed)
+    num_clients = args.num_users
+    dir_path = f"Cifar100_{args.partition}_{args.alpha}_{args.balance}_{args.num_users}/"
+
+
+    if not os.path.exists(dir_path):
+        os.makedirs(dir_path)
+        
+    # Setup directory for train/test data
+    config_path = dir_path + "config.json"
+    train_path = dir_path + "train/"
+    test_path = dir_path + "test/"
+
+    if check(config_path, train_path, test_path, num_clients, niid, balance, partition):
+        return
+        
+    # Get Cifar100 data
+    transform = transforms.Compose(
+        [transforms.ToTensor(), transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+
+    trainset = torchvision.datasets.CIFAR100(
+        root="rawdata/Cifar100", train=True, download=True, transform=transform)
+    testset = torchvision.datasets.CIFAR100(
+        root="rawdata/Cifar100", train=False, download=True, transform=transform)
+    trainloader = torch.utils.data.DataLoader(
+        trainset, batch_size=len(trainset.data), shuffle=False)
+    testloader = torch.utils.data.DataLoader(
+        testset, batch_size=len(testset.data), shuffle=False)
+
+    # record the class names in the dataset
+    classes = trainset.classes
+
+    for _, train_data in enumerate(trainloader, 0):
+        trainset.data, trainset.targets = train_data
+    for _, test_data in enumerate(testloader, 0):
+        testset.data, testset.targets = test_data
+
+    dataset_image = []
+    dataset_label = []
+
+    dataset_image.extend(trainset.data.cpu().detach().numpy())
+    dataset_image.extend(testset.data.cpu().detach().numpy())
+    dataset_label.extend(trainset.targets.cpu().detach().numpy())
+    dataset_label.extend(testset.targets.cpu().detach().numpy())
+    dataset_image = np.array(dataset_image)
+    dataset_label = np.array(dataset_label)
+
+    num_classes = len(set(dataset_label))
+    print(f'Number of classes: {num_classes}')
+
+    # dataset = []
+    # for i in range(num_classes):
+    #     idx = dataset_label == i
+    #     dataset.append(dataset_image[idx])
+
+    X, y, statistic = separate_data((dataset_image, dataset_label), num_clients, num_classes,
+                                    niid, balance, partition, alpha=args.alpha)
+    train_data, test_data = split_data(X, y)
+    save_file(config_path, train_path, test_path, train_data, test_data, num_clients, num_classes, 
+        statistic, niid, balance, partition, args.alpha, classes)
+
+
+if __name__ == "__main__":
+    args = args_parser()
+    niid = True if args.niid == "noniid" else False
+    balance = True if args.balance == "balance" else False
+    partition = args.partition if args.partition != "-" else None
+
+    generate_dataset(niid, balance, partition, args)
---
+++ dataset/generate_TinyImagenet.py
@@ -0,0 +1,147 @@
+# PFLlib: Personalized Federated Learning Algorithm Library
+# Copyright (C) 2021  Jianqing Zhang
+
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+import numpy as np
+import os
+import sys
+import random
+import torch
+import torchvision
+import torchvision.transforms as transforms
+from utils.dataset_utils import check, separate_data, split_data, save_file
+from torchvision.datasets import ImageFolder, DatasetFolder
+import pandas as pd
+
+
+# https://github.com/QinbinLi/MOON/blob/6c7a4ed1b1a8c0724fa2976292a667a828e3ff5d/datasets.py#L148
+class ImageFolder_custom(DatasetFolder):
+    def __init__(self, root, dataidxs=None, train=True, transform=None, target_transform=None):
+        self.root = root
+        self.dataidxs = dataidxs
+        self.train = train
+        self.transform = transform
+        self.target_transform = target_transform
+
+        imagefolder_obj = ImageFolder(self.root, self.transform, self.target_transform)
+        print(imagefolder_obj.class_to_idx)
+        self.loader = imagefolder_obj.loader
+        if self.dataidxs is not None:
+            self.samples = np.array(imagefolder_obj.samples)[self.dataidxs]
+        else:
+            self.samples = np.array(imagefolder_obj.samples)
+
+    def __getitem__(self, index):
+        path = self.samples[index][0]
+        target = self.samples[index][1]
+        target = int(target)
+        sample = self.loader(path)
+        if self.transform is not None:
+            sample = self.transform(sample)
+        if self.target_transform is not None:
+            target = self.target_transform(target)
+
+        return sample, target
+
+    def __len__(self):
+        if self.dataidxs is None:
+            return len(self.samples)
+        else:
+            return len(self.dataidxs)
+
+
+from args import args_parser
+
+
+# Allocate data to users
+def generate_dataset(niid, balance, partition, args):
+    random.seed(args.seed)
+    np.random.seed(args.seed)
+    num_clients = args.num_users
+    dir_path = f"TinyImagenet_{args.partition}_{args.alpha if args.partition == 'dir' else args.class_per_client}_{args.balance}_{args.num_users}/"
+
+    if not os.path.exists(dir_path):
+        os.makedirs(dir_path)
+
+    # Setup directory for train/test data
+    config_path = dir_path + "config.json"
+    train_path = dir_path + "train/"
+    test_path = dir_path + "test/"
+
+    if check(config_path, train_path, test_path, num_clients, niid, balance, partition):
+        return
+
+    # Get data
+    if not os.path.exists(f'rawdata/TinyImagenet/'):
+        os.system(f'wget --directory-prefix rawdata/TinyImagenet/ http://cs231n.stanford.edu/tiny-imagenet-200.zip')
+        os.system(f'unzip rawdata/TinyImagenet/tiny-imagenet-200.zip -d {dir_path}/rawdata/')
+    else:
+        print('rawdata already exists.\n')
+
+    transform = transforms.Compose(
+        [transforms.ToTensor(), transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+
+    trainset = ImageFolder_custom(root='rawdata/TinyImagenet/tiny-imagenet-200/train/', transform=transform)
+    testset = ImageFolder_custom(root='rawdata/TinyImagenet/tiny-imagenet-200/val/', transform=transform)
+    trainloader = torch.utils.data.DataLoader(
+        trainset, batch_size=len(trainset), shuffle=False)
+    testloader = torch.utils.data.DataLoader(
+        testset, batch_size=len(testset), shuffle=False)
+
+    # record the class names in the dataset
+    csv_path = "rawdata/TinyImagenet//tiny-imagenet-200/tinyimagenet_classes_sorted_lex.csv"  # 替换为实际路径
+    df = pd.read_csv(csv_path)
+
+    # Extract class labels into a list
+    classes = df['Class Label'].tolist()
+
+    for _, train_data in enumerate(trainloader, 0):
+        trainset.data, trainset.targets = train_data
+    for _, test_data in enumerate(testloader, 0):
+        testset.data, testset.targets = test_data
+
+    dataset_image = []
+    dataset_label = []
+
+    dataset_image.extend(trainset.data.cpu().detach().numpy())
+    dataset_image.extend(testset.data.cpu().detach().numpy())
+    dataset_label.extend(trainset.targets.cpu().detach().numpy())
+    dataset_label.extend(testset.targets.cpu().detach().numpy())
+    dataset_image = np.array(dataset_image)
+    dataset_label = np.array(dataset_label)
+
+    num_classes = len(set(dataset_label))
+    print(f'Number of classes: {num_classes}')
+
+    # dataset = []
+    # for i in range(num_classes):
+    #     idx = dataset_label == i
+    #     dataset.append(dataset_image[idx])
+
+    X, y, statistic = separate_data((dataset_image, dataset_label), num_clients, num_classes,
+                                    niid, balance, partition, alpha=args.alpha)
+    train_data, test_data = split_data(X, y)
+    save_file(config_path, train_path, test_path, train_data, test_data, num_clients, num_classes,
+              statistic, niid, balance, partition, args.alpha, classes)
+
+
+if __name__ == "__main__":
+    args = args_parser()
+    niid = True if args.niid == "noniid" else False
+    balance = True if args.balance == "balance" else False
+    partition = args.partition if args.partition != "-" else None
+
+    generate_dataset(niid, balance, partition, args)
